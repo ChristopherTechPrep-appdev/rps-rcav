afb3d5a260aa628c51e27d863e60ab10b4ea30a0
ChristopherTechPrep <cmazon32@gmail.com>
2023-02-27 21:31:10 +0000

    @ruby tings @outcome @comp_move

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def homepage
-    render({ :template => "game_templates/rules.html.erb"})
+    render({ :template => "game_templates/rules.html.erb" })
   end
 
   def play_rock
@@ -15,6 +15,26 @@ class ApplicationController < ActionController::Base
 
     # render({ :html => "<h1>Hello, World! </h1>".html_safe })
 
-    render ({ :template => "game_templates/user_rock.html.erb"})
+    render ({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/paper.html.erb" })
+  end
+
+  def play_scissors
+    
+
+    render({ :template => "game_templates/scissors.html.erb" })
   end
 end
